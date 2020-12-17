937b6b62631f5a47b962d84e4bcf36bab0af0ca5
Christian Ambang <dc.ambang@ecole-ipssi.net>
2020-12-17 16:15:16 +0100

    Uptade2 debug eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+
+        ,"allowImportExportEverywhere": true,
+        "codeFrame": true
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
